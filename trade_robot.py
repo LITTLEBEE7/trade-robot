63a3ebf8d13d657475444f8680d38194f1b2602e
notgood0937 <notgoood0937@gmail.com>
2023-02-17 23:31:37 +0800

    update

--- trade_robot.py
+++ trade_robot.py
@@ -379,7 +379,7 @@ def start_trade():
 if __name__ == '__main__':
     try:
         # 启动服务
-        app.run(port=8080)
+        app.run()
     except Exception as e:
         logging.info(e)
         pass
