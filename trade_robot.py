456bbfb9f6aff5f7e8389572a5ccdb6532b5227a
“myway7” <wangm847@gmail.com>
2023-03-07 15:36:19 +0800

    update log

--- trade_robot.py
+++ trade_robot.py
@@ -38,10 +38,10 @@ def setup_log(log_name):
     # when="MIDNIGHT", interval=1 表示每天0点为更新点，每天生成一个文件
     # backupCount  表示日志保存个数
     file_handler = TimedRotatingFileHandler(
-        filename=log_path, when="S", interval=1, backupCount=30
+        filename=log_path, when="MIDNIGHT", interval=1, backupCount=30
     )
     # filename="mylog" suffix设置，会生成文件名为mylog.2020-02-25.log
-    file_handler.suffix = "%Y-%m-%d-%H-%M-%S.log"
+    file_handler.suffix = "%Y-%m-%d.log"
     # extMatch是编译好正则表达式，用于匹配日志文件名后缀
     # 需要注意的是suffix和extMatch一定要匹配的上，如果不匹配，过期日志不会被删除。
     file_handler.extMatch = re.compile(r"^\d{4}-\d{2}-\d{2}.log$")
@@ -54,7 +54,7 @@ def setup_log(log_name):
     logger.addHandler(file_handler)
     return logger
 # 初始化日志
-logger = setup_log("my_trade.log")
+logger = setup_log("my_trade")
 # 读取设置
 config = {}
 if os.path.exists('./config.json'):
